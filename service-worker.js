eefdf9b79e0a3d8619aa8692e06106cde4a4bb40
aretheregods <aretheregods@gmail.com>
2016-12-28 06:37:17 -0500

    Updated precache code

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,30 @@
 importScripts('/bower_components/sw-toolbox/sw-toolbox.js');
 
+// Version 2
+
+toolbox.precache(['/sounds/iloveyou.wav',
+				  '/sounds/iwantyou.wav',
+				  '/sounds/imissyou.wav',
+				  '/sounds/youresobeautiful.wav',
+				  '/sounds/youremybaby.wav',
+				  '/sounds/youremyangel.wav',
+				  '/sounds/cathedral.wav',
+				  '/sounds/beach.wav',
+				  '/sounds/tunnel.wav',
+				  '/sounds/cavern.wav',
+				  '/sounds/metro.wav',
+				  '/sounds/library.wav'
+				  ],
+				  ['/icons/tre.png',
+				   '/icons/favicons-16x16.png',
+				   '/icons/favicons-32x32.png',
+				   '/icons/favicon.ico'
+				  ],
+				  ['/bower_components/sw-toolbox/sw-toolbox.js',
+				   '/bower_components/skeleton/css/normalize.css',
+				   '/bower_components/skeleton/css/skeleton.css'],
+				  ['/index.html',
+				   '/404.html'],
+				  ['/tremalenkislova.js']);
+
 toolbox.router.default = toolbox.cacheFirst;
